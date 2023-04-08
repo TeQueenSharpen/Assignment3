141cebb8a59fe28a2eed43a1db062542c2f32e79
TeQueenSharpen <124606580+TeQueenSharpen@users.noreply.github.com>
2023-04-07 22:26:09 -0700

    Add files via upload

---
+++ DFS MeowWolf/script.js
@@ -0,0 +1,72 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+window.onscroll = function() {stickyFunction()};
+
+var topnav = document.getElementById("myTopnav");
+
+var sticky = topnav.offsetTop;
+
+function stickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    myTopnav.classList.add("sticky")
+  } else {
+    myTopnav.classList.remove("sticky");
+  }
+}
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function phoneNavBar() {
+  
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    document.documentElement.scrollTop = 0
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
